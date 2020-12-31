9085d99f96ae0a933f71e26467d9068c95208578
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-31 11:41:05 +0100

    Use subshell (instead of backtick) and a more precise regex

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-GCOV_EXE=gcov
-
-OBJ_FILES_PATH=.
-
-if [ ! -d "$OBJ_FILES_PATH" ]
-then
-  echo "Object file path not found at "$OBJ_FILES_PATH
-  exit 1
-fi
-
-for filename in `find . | egrep '^.*\.cpp$' | egrep -v "^.*_test\.cpp$"`; do $GCOV_EXE -n -o $OBJ_FILES_PATH $filename > /dev/null; done
+for filename in $(find . | egrep '\.cpp$');
+do 
+  gcov -n -o . $filename > /dev/null; 
+done
